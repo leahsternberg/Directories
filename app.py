84456f4f68c15d45ab5b03c019b390aa9c926e0f
leahsternberg <63926660+leahsternberg@users.noreply.github.com>
2020-07-12 09:39:19 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+path = Path("ecommerce")
+# path.exists()
+# path.mkdir()
+# path.rmdir()
+# path.rename("ecommerce2")
+
+paths = [p for p in path.iterdir() if p.is_dir]
+py_files = [p for p in path.rglob("*.py")]
+print(py_files)
